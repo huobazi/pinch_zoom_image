e443c6f8e4cc579daf462f95ffbb23213bb60b77
Marble Wu <huobazi@gmail.com>
2020-05-07 14:49:30 +0800

    bump compilesSdkversion

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mw82/projects/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mw82/projects/pinch_zoom_image/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mw82/projects/flutter/bin/cache/artifacts/engine/ios"
